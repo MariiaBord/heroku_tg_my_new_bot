7a89c1bcf164e8541c3a34da8d0de72ac602f72b
MariiaBord <masha555@ukr.net>
2019-02-12 21:02:49 +0200

    better

--- My_new_bot.py
+++ My_new_bot.py
@@ -5,7 +5,7 @@ import misc
 import Min_fin
 from boto.s3.connection import S3Connection
 
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+#s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 bot = telebot.TeleBot(misc.token)
 
 
